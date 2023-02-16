fa65e8763fc4418a3b504742a5131751c80c253b
ruscias <stevenwrusciano@gmail.com>
2023-02-16 16:44:52 -0600

    Add ESLint configurations

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
-function Book (title, author, pages, completed=false) {
+function Book(title, author, pages, completed = false) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.completed = completed;
 }
 
-Book.prototype.changeCompleted = function () {
+Book.prototype.changeCompleted = function() {
   this.completed = this.completed ? false : true;
-}
+};
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien','295');
-const tuesdays = new Book('Tuesdays with Morrie', 'Mitch Albom','192', true);
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295');
+const tuesdays = new Book('Tuesdays with Morrie', 'Mitch Albom', '192', true);
 
 const books = [];
 
@@ -21,7 +21,11 @@ const tableBody = document.querySelector('tbody');
 
 updateTable();
 
-function updateTable(){
+function clearTable() {
+  console.log('yep');
+}
+
+function updateTable() {
   for (const book of books) {
     const newRow = tableBody.appendChild(document.createElement('tr'));
     for (const prop in book) {
@@ -42,7 +46,7 @@ function updateTable(){
 
 function success() {
   console.log(`${this.innerText} success`);
-};
+}
 
 function updateBook(title) {
   for (const book of books) {
@@ -53,7 +57,7 @@ function updateBook(title) {
 }
 
 function statusInput() {
-  const title = this.parentElement.parentElement.firstElementChild.innerText; 
+  const title = this.parentElement.parentElement.firstElementChild.innerText;
   updateBook(title);
   updateTable();
 }
@@ -77,10 +81,10 @@ deleteAllButton.addEventListener('click', success);
 // This place is basically a pile of code without comments or git messages
 // Trying to communicate next steps...
 // Well, I am trying to get a pop up when I hit add that will take...
-//...the parameters needed to create a new Book object
+// ...the parameters needed to create a new Book object
 // I'd like to push that new Book to the books array
 // Then I suppose you might use the updateTable function to update the table...
-//...sounds too easy, I agree. Let's see what happens...
+// ...sounds too easy, I agree. Let's see what happens...
 // I'd like the delete all button to delete everything in that array
 // I guess if that all works... the assignment is done...
 // I have lots of feature ideas
